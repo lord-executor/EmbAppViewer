8993cbb56f0fc255d752fd095fce6301ab9b9784
lord-executor <executor.dev@gmail.com>
2019-07-26 16:50:55 +0200

    Added IPC based command line passing to instances of EmbAppViewer

--- src/EmbAppViewer/App.xaml.cs
+++ src/EmbAppViewer/App.xaml.cs
@@ -1,9 +1,7 @@
-﻿using System;
+﻿using EmbAppViewer.InterComm;
+using System;
 using System.Collections.Generic;
 using System.Configuration;
-using System.Data;
-using System.Linq;
-using System.Threading.Tasks;
 using System.Windows;
 
 namespace EmbAppViewer
@@ -11,7 +9,27 @@ namespace EmbAppViewer
     /// <summary>
     /// Interaction logic for App.xaml
     /// </summary>
-    public partial class App : Application
+    public partial class App : Application, ICommandLineReceiver
     {
+
+        private void Application_Startup(object sender, StartupEventArgs e)
+        {
+            if (CommandLineInterComm<App>.TrySendCommandLineArgs(ConfigurationManager.AppSettings.Get("ApplicationIdentifier"), "Test", Environment.GetCommandLineArgs()))
+            {
+                Shutdown();
+            }
+        }
+
+        private void Application_Exit(object sender, ExitEventArgs e)
+        {
+            CommandLineInterComm<App>.Cleanup();
+        }
+
+        public bool ReceiveCommandLineArgs(IList<string> args)
+        {
+            // handle command line arguments of second instance
+            // ...
+            return true;
+        }
     }
 }
